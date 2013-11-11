5cf338fa9c4b0e5e83c9cc2494c639c5990ebb66
RighteousRyn <ryanbyee@gmail.com>
2013-11-10 22:47:50 -0800

    Passed all cases

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,19 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+var results = [];
+var element = arguments[1] || document.body;
+var traverseElm = function(element) {
+	return getElementsByClassName(className,element)[0]; 
+};
+var checkClassname = results.indexOf.call(element.classList, className) !== -1;
+var childrenResults = results.map.call(element.children, traverseElm).filter(Boolean);
+
+if (checkClassname) {
+  results.push(element);
+} else {
+  results = results.concat(childrenResults);
+}
+
+return results;
 };
